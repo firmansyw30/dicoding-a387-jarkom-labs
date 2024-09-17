6d9331927e83f669d4a1c20b1a8de0d55d6077b6
Firmansyah Wicaksono <firmansyahwicaksono30@gmail.com>
2024-09-17 07:28:18 +0700

    1st commit

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const PORT = 8000
 const HOST = '0.0.0.0'
 
 const app = express()
+app.use(express.json()); // To parse JSON requests
+
 /*
 [ROUTE] '/'
 [Response] => Hello world!
@@ -18,6 +20,19 @@ app.get('/', (req, res) => {
 Optional [ROUTE] '/me'
 [Response] => Dicoding username.
 */
+app.get('/me', (req, res) => {
+    res.send('dicoding_username : firmansyw30 \n')
+})
+
+app.get('/usr', (req, res) => {
+    // Retrieve the username from query parameters or headers
+    const username = req.query.username || req.headers.username;
+    if (!username) {
+        res.status(401).send('Unauthorized: No username provided');
+    } else {
+        res.send(`Hello, ${username}!\n`);
+    }
+})
 
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
