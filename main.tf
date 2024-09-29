77366c7963de6a40702200aaf2ed79c37a9a6ebf
Firmansyah Wicaksono <firmansyahwicaksono30@gmail.com>
2024-09-29 15:25:47 +0700

    update user data ec2

--- main.tf
+++ main.tf
@@ -87,11 +87,12 @@ resource "aws_route_table" "route-table-b" {
   tags = {
     Name = "Route-Table-B"
   }
+}
 
-  route {
-    cidr_block = "10.0.0.0/16"
-    gateway_id = null
-  }
+# Associate the private route table with Subnet B
+resource "aws_route_table_association" "route-table-association-b" {
+  subnet_id      = aws_subnet.subnet-b.id
+  route_table_id = aws_route_table.route-table-b.id
 }
 
 
@@ -146,6 +147,7 @@ resource "aws_instance" "web_server" {
   subnet_id     = aws_subnet.subnet-a.id
   associate_public_ip_address = true
   key_name      = "sample-key-pair-firman" # ensure this key pair available
+  vpc_security_group_ids = [aws_security_group.web-server-sg.id]  # attach security group
 
   tags = {
     Name = "Test-Web-Server-Instance-Firmansyw30"
@@ -155,8 +157,8 @@ resource "aws_instance" "web_server" {
   user_data = <<-EOF
     #!/bin/bash
     # Update and install necessary packages
-    sudo yum update -y
-    sudo yum install -y git nginx
+    sudo apt-get update -y
+    sudo apt-get install -y git nginx
 
     # Start and enable nginx
     sudo systemctl start nginx
@@ -167,33 +169,15 @@ resource "aws_instance" "web_server" {
     cd dicoding-a387-jarkom-labs
 
     # Install nvm and Node.js
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-    source ~/.nvm/nvm.sh
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     nvm install v14.15.4
 
     # Install dependencies and start Node.js application
-    npm install
-    npm run start
+    sudo npm install
+    sudo npm run start
 
-    # Configure Nginx as reverse proxy
-    sudo cat > /etc/nginx/conf.d/default.conf <<EOL
-    server {
-        listen 80;
-        server_name localhost;
-
-        location / {
-            proxy_pass http://localhost:8000;
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection 'upgrade';
-            proxy_set_header Host $host;
-            proxy_cache_bypass $http_upgrade;
-        }
-    }
-    EOL
-
-    # Restart Nginx to apply the configuration
-    sudo systemctl restart nginx
   EOF
 }
 
